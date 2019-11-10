d8b387e7b62cfb6c57aca8759be1c5e294a93b7b
tracy-s <57414450+tracy-s@users.noreply.github.com>
2019-11-10 18:18:21 -0500

    Update 191105_RUN_AERMOD.py

--- 191105_RUN_AERMOD.py
+++ 191105_RUN_AERMOD.py
@@ -1,22 +1,24 @@
 ## Nov 5th 2019
 ## Let's Run AERMOD
 
+#while true   ##should run infinitely
+
 import xlwt  ##allows python to work with excel
-AERMET_FSC = xlwt.AERMET()
 
-while true   ##should run infinitely
+import datetime
+date_time_now = datetime.datetime.now()   ##current date and time
+hour_now = date_time_now.hour             ##pulls hour from date_time_now - to be used as index for for loop
 
-  import datetime
-  date_time = datetime.datetime.now()   ##current date and time
+for hour_now =/= hour_prev_run
 
   import os
   import subprocess
 
-  os.chdir("C:/Users/Alex/Documents/Capstone Macaroni/PCLI AERMOD FILES")
+  os.chdir("C:/Users/Alex/Documents/Capstone Macaroni/PCLI AERMOD FILES")  ##directory needs to be made to a general link
 
   command = "AERMOD"
   subprocess.Popen(command)
 
-  AERMOD_output = book.add_sheet("AERMOD outputs") ##saves outputs to Excel for the time stamp
+  AERMOD_output = book.add_sheet("AERMOD outputs") ##saves outputs to Excel for the time stamp. Not done yet
 
 
