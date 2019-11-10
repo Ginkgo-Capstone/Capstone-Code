30e2a96e79a45997325fa55bfa59fbfbe916f697
tracy-s <57414450+tracy-s@users.noreply.github.com>
2019-11-10 18:05:52 -0500

    Update 191105_RUN_AERMOD.py

--- 191105_RUN_AERMOD.py
+++ 191105_RUN_AERMOD.py
@@ -2,23 +2,21 @@
 ## Let's Run AERMOD
 
 import xlwt  ##allows python to work with excel
+AERMET_FSC = xlwt.AERMET()
 
 while true   ##should run infinitely
 
-from datetime import datetime
-date_time = datetime.fromtimestamp(timestamp)
+  import datetime
+  date_time = datetime.datetime.now()   ##current date and time
 
-import os
-import subprocess
+  import os
+  import subprocess
 
-os.chdir("C:/Users/Alex/Documents/Capstone Macaroni/PCLI AERMOD FILES")
+  os.chdir("C:/Users/Alex/Documents/Capstone Macaroni/PCLI AERMOD FILES")
 
+  command = "AERMOD"
+  subprocess.Popen(command)
 
-command = "AERMOD"
-
-subprocess.Popen(command)
-
-
-AERMOD_output = book.add_sheet("AERMOD outputs") ##saves outputs to Excel for the time stamp
+  AERMOD_output = book.add_sheet("AERMOD outputs") ##saves outputs to Excel for the time stamp
 
 
